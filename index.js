c288fbf16778cfe92a70a7b52e3d25f8da58a44f
Ilya <kotyrov.1310@gmail.com>
2022-12-26 21:45:00 +0300

    make grid

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 const gridContainer = document.getElementById('draw-space');
+
+function makeGrid(height, width) {
+    const grid = document.createElement('div');
+    grid.classList.add('grid');
+    grid.style.setProperty('grid-template-columns', `repeat(${width}, 1fr)`);
+    grid.style.setProperty('grid-template-rows', `repeat(${height}, 1fr)`);
+    for (let i = 0; i < width * height; ++i) {
+        const cell = document.createElement('div');
+        cell.classList.add('grid-cell');
+
+        grid.appendChild(cell);
+    }
+    return grid;
+}
+
+gridContainer.appendChild(makeGrid(16, 16));
